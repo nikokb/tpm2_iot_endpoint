c8de8e6b48ca29e0f5c7154ca9b1c6f531ea88f2
nikokb <66697214+nikokb@users.noreply.github.com>
2020-06-09 12:39:29 -0700

    Add files via upload

--- tpm_persist_private.sh
+++ tpm_persist_private.sh
@@ -4,3 +4,4 @@ tpm2_import -C primary.ctx -G rsa -i private.pem -u key.pub -r key.prv
 rm private.pem
 tpm2_load -C primary.ctx -u key.pub -r key.prv -c key.ctx
 tpm2_evictcontrol -C o -c key.ctx 0x81010002
+./gcs_auth test
